f6ef958aebf268179529230f197de3a849523e00
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-07-29 16:29:10 +0900

    #2.1 cacheImages

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, Image } from "react-native";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+
+const cacheImages = (images) =>
+  images.map((img) => {
+    if (typeof img === "string") {
+      return Image.prefetch(img);
+    } else {
+      return Asset.fromModule(img).downloadAsync();
+    }
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1593642634627-6fdaf35209f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      require("./assets/splash.png"),
+    ]);
+    console.log(images);
+    // expected output:
+    // [
+    //   Promise {
+    //     "_40": 0,
+    //     "_55": null,
+    //     "_65": 0,
+    //     "_72": null,
+    //   },
+    //   Promise {
+    //     "_40": 0,
+    //     "_55": null,
+    //     "_65": 0,
+    //     "_72": null,
+    //   },
+    // ]
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ? (
+    <Text>Ready!</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    ></AppLoading>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
