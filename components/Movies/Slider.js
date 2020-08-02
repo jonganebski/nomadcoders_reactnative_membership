e3b3534233d4e6aab20199c8617f596dc9960252
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-08-02 19:47:52 +0900

    #4.3 Slider Content

--- components/Movies/Slider.js
+++ components/Movies/Slider.js
@@ -1,25 +1,54 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { Dimensions } from "react-native";
 import { apiImage } from "../../api";
 
-const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
-
 const Container = styled.View`
-  width: ${WIDTH}px;
-  height: ${HEIGHT / 4}px;
-  background-color: red;
+  width: 100%;
+  height: 100%;
 `;
 
 const BG = styled.Image`
   width: 100%;
   height: 100%;
+  opacity: 0.6;
+  position: absolute;
+`;
+
+const Content = styled.View`
+  flex-direction: row;
+`;
+
+const Data = styled.View`
+  width: 50%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const Votes = styled.Text`
+  color: white;
+  opacity: 0.7;
+`;
+
+const Overview = styled.Text`
+  color: white;
+  opacity: 0.7;
 `;
 
 const Slide = ({ id, title, backgroundImage, votes, overview }) => (
   <Container>
     <BG source={{ uri: apiImage(backgroundImage) }}></BG>
+    <Content>
+      <Data>
+        <Title>{title}</Title>
+        <Votes>{votes} / 10</Votes>
+        <Overview>{overview}</Overview>
+      </Data>
+    </Content>
   </Container>
 );
 
--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, Dimensions } from "react-native";
 import Slide from "../../components/Movies/Slider";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   background-color: black;
+`;
+
+const SliderContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${HEIGHT / 4}px;
 `;
 
 const MoviesPresenter = ({ isLoading, nowPlaying }) => {
@@ -16,20 +23,20 @@ const MoviesPresenter = ({ isLoading, nowPlaying }) => {
       {isLoading ? (
         <ActivityIndicator color="white" size="small"></ActivityIndicator>
       ) : (
-        <>
+        <SliderContainer>
           <Swiper controlsEnabled={false} loop timeout={3}>
             {nowPlaying.map((movie) => (
               <Slide
                 key={movie.id}
                 id={movie.id}
-                title={movie.original_language}
+                title={movie.original_title}
                 backgroundImage={movie.backdrop_path}
                 votes={movie.vote_average}
                 overview={movie.overview}
               ></Slide>
             ))}
           </Swiper>
-        </>
+        </SliderContainer>
       )}
     </Container>
   );
