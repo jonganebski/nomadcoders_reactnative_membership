0397ad72800a32dc150a6911eebc6d2a0d0ae5eb
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-08-04 21:10:52 +0900

    #6.1 Searching Logic

--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,14 +1,31 @@
 import React from "react";
 import styled from "styled-components/native";
 import Input from "../../components/Search/Input";
+import HorizontalSlide from "../../components/HorizontalSlide";
+import Horizontal from "../../components/Horizontal";
 
 const Container = styled.ScrollView`
   background-color: black;
 `;
 
-const SerachPresenter = () => (
+const SerachPresenter = ({ movies, shows, keyword, onChange, onSubmit }) => (
   <Container>
-    <Input placeholder="Write a keyword"></Input>
+    <Input
+      placeholder="Write a keyword"
+      value={keyword}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    ></Input>
+    <HorizontalSlide title="Movie Results">
+      {movies.map((movie) => (
+        <Horizontal></Horizontal>
+      ))}
+    </HorizontalSlide>
+    <HorizontalSlide title="TV Shows Results">
+      {shows.map((show) => (
+        <Horizontal></Horizontal>
+      ))}
+    </HorizontalSlide>
   </Container>
 );
 
